2a0699b772b6ccb39080ecdee6626e30f9a8c9dd
Ahm3dmohamed <you@example.com>
2025-02-14 02:17:08 +0200

    add payment bottom sheet

--- lib/features/checkout/presentation/views/my_cart_view.dart.dart
+++ lib/features/checkout/presentation/views/my_cart_view.dart.dart
@@ -4,6 +4,7 @@ import 'package:payment/core/resources/values_manager.dart';
 import 'package:payment/core/widgets/build_appbar.dart';
 import 'package:payment/core/widgets/checkout_button.dart';
 import 'package:payment/features/checkout/presentation/views/payment_details_view.dart';
+import 'package:payment/features/checkout/widgets/payment_methods_bootomsheet.dart';
 import 'package:payment/features/checkout/widgets/payment_methods_listview.dart';
 import 'package:payment/features/checkout/widgets/product_image.dart';
 import 'package:payment/features/checkout/widgets/product_info.dart';
@@ -68,35 +69,6 @@ class _MyCartViewState extends State<MyCartView> {
               );
             },
             total: total,
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class PaymentMethodsBottomSheet extends StatelessWidget {
-  const PaymentMethodsBottomSheet({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        children: [
-          PaymentMethods(),
-          const SizedBox(height: AppSize.s16),
-          const Spacer(),
-          CheckoutButton(
-            text: "Continue",
-            onPressed: () {
-              // Navigator.push(
-              //     context,
-              //     MaterialPageRoute(
-              //       builder: (context) => const PaymentDetailsView(),
-              //     ));
-            },
-            total: 0,
           ),
         ],
       ),
