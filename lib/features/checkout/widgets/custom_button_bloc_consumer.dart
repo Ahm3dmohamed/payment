5660c7d02cdd149a5c71a9c99dc98898a7f06b64
Ahm3dmohamed <you@example.com>
2025-02-16 03:50:22 +0200

    create amount model create items model

--- lib/features/checkout/widgets/custom_button_bloc_consumer.dart
+++ lib/features/checkout/widgets/custom_button_bloc_consumer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_paypal_payment/flutter_paypal_payment.dart';
 import 'package:payment/core/widgets/checkout_button.dart';
 import 'package:payment/features/checkout/data/models/payment_intent_input_model.dart';
 import 'package:payment/features/checkout/presentation/manager/cubit/payment_cubit.dart';
@@ -32,15 +33,80 @@ class CustomBottonBlocConsumer extends StatelessWidget {
           isLoading: state is PaymentLoading,
           text: "Continue",
           onPressed: () {
-            PaymentIntentInputModel paymentIntentInputModel =
-                PaymentIntentInputModel(
-              customerId: "cus_RmOzspT2Pefanh",
-              amount: "100",
-              currency: "USD",
-            );
+            // PaymentIntentInputModel paymentIntentInputModel =
+            //     PaymentIntentInputModel(
+            //   customerId: "cus_RmOzspT2Pefanh",
+            //   amount: "100",
+            //   currency: "USD",
+            // );
 
-            BlocProvider.of<PaymentCubit>(context)
-                .doPayment(paymentIntentInputModel);
+            // BlocProvider.of<PaymentCubit>(context)
+            //     .doPayment(paymentIntentInputModel);
+
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (BuildContext context) => PaypalCheckoutView(
+                sandboxMode: true,
+                clientId: "",
+                secretKey: "",
+                transactions: const [
+                  {
+                    "amount": {
+                      "total": "100",
+                      "currency": "USD",
+                      "details": {
+                        "subtotal": "100",
+                        "shipping": "0",
+                        "shipping_discount": "0",
+                      }
+                    },
+                    "description": "The payment transaction description.",
+                    // "payment_options": {
+                    //   "allowed_payment_method":
+                    //       "INSTANT_FUNDING_SOURCE"
+                    // },
+                    "item_list": {
+                      "items": [
+                        {
+                          "name": "Apple",
+                          "quantity": 4,
+                          "price": '5',
+                          "currency": "USD"
+                        },
+                        {
+                          "name": "Pineapple",
+                          "quantity": 5,
+                          "price": '10',
+                          "currency": "USD"
+                        }
+                      ],
+
+                      // shipping address is not required though
+                      //   "shipping_address": {
+                      //     "recipient_name": "tharwat",
+                      //     "line1": "Alexandria",
+                      //     "line2": "",
+                      //     "city": "Alexandria",
+                      //     "country_code": "EG",
+                      //     "postal_code": "21505",
+                      //     "phone": "+00000000",
+                      //     "state": "Alexandria"
+                      //  },
+                    }
+                  }
+                ],
+                note: "Contact us for any questions on your order.",
+                onSuccess: (Map params) async {
+                  print("onSuccess: $params");
+                },
+                onError: (error) {
+                  print("onError: $error");
+                  Navigator.pop(context);
+                },
+                onCancel: () {
+                  print('cancelled:');
+                },
+              ),
+            ));
           },
           total: 100,
         );
